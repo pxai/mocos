ce33b59fcfc3d27423776469f2e007386f0d45fa
pxai <pello.altadill@gluckgames.com>
2020-06-30 20:50:16 +0200

    Added sample

---
+++ src/00/ejercicios/01.py
@@ -0,0 +1,7 @@
+nombre = input("Introduce tu nombre: ")
+print("Hola, qué tal estás ", nombre)
+# print("Hola, qué tal estás %s" % nombre)
+nombre = "Juan"
+edad = 34
+print("Tu nombre es", nombre, ", tienes", edad, "años")
+# print("Tu nombre es %s, tienes %d años" % (nombre, edad))
