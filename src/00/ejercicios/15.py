07f6b9372f37fdf9347d292ffc6c5680bd390c0f
pxai <pello.altadill@gluckgames.com>
2020-07-15 09:20:09 +0200

    Added moar samples

---
+++ src/00/ejercicios/15.py
@@ -0,0 +1,6 @@
+valor1 = input("Introduce un número: ")
+valor2 = input("Introduce otro número: ")
+
+resultado = (int(valor1) >= 0) or (int(valor2) >= 0)
+
+print("¿Es alguno de los dos positivo?", resultado)
