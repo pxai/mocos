fcce0b77e8b13a18b9cb8f8aeb51e90546d153d8
pxai <pello.altadill@gluckgames.com>
2020-07-19 11:24:03 +0200

    Added moar samples

---
+++ src/01/ejercicios/19.py
@@ -0,0 +1,10 @@
+texto = input("Introduce un texto: ")
+
+if texto == "mañana":
+    print("Buenos días.")
+elif texto == "tarde":
+    print("Buenas tardes.")
+elif texto == "noche":
+    print("Buenas noches.")
+else:
+    print("No entiendo.")
