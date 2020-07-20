02436bbcc920fa5b3e02caab6419764b62187984
pxai <pello.altadill@gluckgames.com>
2020-07-20 11:24:23 +0200

    Added moar samples

---
+++ src/01/ejercicios/20.py
@@ -0,0 +1,28 @@
+mes = input("Introduce un mes del año: ")
+
+if mes == "Enero":
+    print("31 días")
+elif mes == "Febrero":
+    print("28/29 días")
+elif mes == "Marzo":
+    print("31 días")
+elif mes == "Abril":
+    print("30 días")
+elif mes == "Mayo":
+    print("31 días")
+elif mes == "Junio":
+    print("30 días")
+elif mes == "Julio":
+    print("31 días")
+elif mes == "Agosto":
+    print("31 días")
+elif mes == "Septiembre":
+    print("30 días")
+elif mes == "Octubre":
+    print("31 días")
+elif mes == "Noviembre":
+    print("30 días")
+elif mes == "Diciembre":
+    print("31 días")
+else:
+    print("Mes desconocido")
