68b3be0731f754de1b717d86ea76a447b06990dc
pxai <pello.altadill@gluckgames.com>
2020-07-21 11:42:09 +0200

    Added moar samples

---
+++ src/01/ejercicios/1.py
@@ -0,0 +1,9 @@
+numero1 = input("Introduce un número: ")
+numero2 = input("Introduce otro número: ")
+
+if numero1 > numero2:
+  print(numero1, " es mayor que ", numero2)
+elif numero1 < numero2:
+  print(numero1, " es menor que ", numero2)
+else:
+  print(numero1, " es igual que ", numero2)
