003cfc67a20fa9a06f62244e98adf7bc777ff12b
pxai <pello.altadill@gluckgames.com>
2020-07-29 10:37:11 +0200

    Added moar samples

---
+++ src/04/ejercicios/1.py
@@ -0,0 +1,12 @@
+def diferencia (valor1, valor2):
+    diferencia = 0
+
+    if valor1 > valor2:
+        diferencia = valor1 - valor2
+    else:
+        diferencia = valor2 - valor1
+
+    print("La diferencia es: ", diferencia)
+
+diferencia(10, 5)
+diferencia(4, 12)
