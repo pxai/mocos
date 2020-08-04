48ba53119cf06dfedba168500fb408780ee05b62
pxai <pello.altadill@gluckgames.com>
2020-08-04 10:38:52 +0200

    Added moar samples

---
+++ src/04/ejercicios/7.py
@@ -0,0 +1,20 @@
+import random
+
+def aleatorio (max):
+    return random.randint(0, max)
+
+
+def generarNombre (silabas):
+    vocales = ["a", "e", "i", "o", "u"]
+    consonantes = ["b","c","d","f","g","h","j","k","l","m","n","ñ","p","q","r","s","t","v","w","x","y","z"]
+    nombre = ""
+
+    for i in range(silabas):
+        consonante = consonantes[aleatorio(len(consonantes))]
+        vocal = vocales[aleatorio(len(vocales))]
+        nombre = nombre + consonante + vocal
+
+    return nombre
+
+
+print(generarNombre(3))
