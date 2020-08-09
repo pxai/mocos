d47741237dcf189bdd4d657696fe0cd0ef83da75
pxai <pello.altadill@gluckgames.com>
2020-08-09 21:02:09 +0200

    Added moar samples

---
+++ src/05/ejercicios/6.py
@@ -0,0 +1,29 @@
+class Vehiculo:
+    def __init__(self, matricula):
+        self._matricula = matricula
+
+    @attribute
+    def matricula (self):
+        return self._matricula
+
+    @matricula.setter
+    def matricula (self, matricula):
+        self._matricula = matricula
+
+    def arrancar (self):
+        print("Arrancando ", self._matricula)
+
+
+class Coche(Vehiculo):
+    def __init__(self, matricula, modelo, color):
+        super().__init__(matricula)
+        self._modelo = modelo
+        self._color = color
+
+    def info (self):
+        return f"{self.matricula} {self._modelo} {self._color}";
+
+
+coche = Coche("0042ASI", "Opel Corsa", "Blanco")
+coche.arrancar()
+print(coche.info())
