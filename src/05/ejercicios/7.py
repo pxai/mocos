db5bc33f4eb71c023daf832630626863b8e6a317
pxai <pello.altadill@gluckgames.com>
2020-08-10 21:02:20 +0200

    Added moar samples

---
+++ src/05/ejercicios/7.py
@@ -0,0 +1,37 @@
+class Piloto:
+    def __init__(selfnombre):
+        self._nombre = nombre
+
+    @attribute
+    def nombre (self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre (self, nombre):
+        self._nombre = nombre
+
+
+
+class Aeroplano:
+    def __init__(self, modelo, piloto, copiloto):
+        self._modelo = modelo
+        self._piloto = piloto
+        self._copiloto = copiloto
+
+    @attribute
+    def modelo (self):
+        return self._modelo
+
+    @modelo.setter
+    def modelo (self, modelo):
+        self._modelo = modelo
+
+    def volar (self):
+        return f"Volando {self._modelo} {self._piloto.nombre} con {self._copiloto.nombre}"
+
+
+piloto1 = Piloto("Han Solo")
+piloto2 = Piloto("Murdock")
+avioneta = Aeroplano("AirBluff 727", piloto1, piloto2)
+
+print(avioneta.volar())
