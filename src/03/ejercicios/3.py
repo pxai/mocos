6eb04cf38b11c5d48161a25a9c4a61feef15f176
pxai <pello.altadill@gluckgames.com>
2020-08-11 21:08:04 +0200

    Added moar samples

---
+++ src/03/ejercicios/3.py
@@ -0,0 +1,15 @@
+numeros = [3, 5, -4, 2, 1, 4, 0, 6, 9, 8, 3]
+
+for numero in numeros:
+    print(numero)
+
+
+for numero in numeros:
+    numero = numero + 1
+
+for numero in numeros:
+    print(numero)
+
+
+# Alternativa para la suma:
+# numerosIncrementados = numeros.map( numero => numero + 1 )
