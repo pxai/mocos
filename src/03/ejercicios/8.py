d0f16602423eb974b7b486a5da75541ed50bd504
pxai <pello.altadill@gluckgames.com>
2020-08-13 21:08:56 +0200

    Added moar samples

---
+++ src/03/ejercicios/8.py
@@ -0,0 +1,7 @@
+nombre = input("Escribe tu nombre: ")
+lugar = input("Escribe tu lugar de nacimiento: ")
+fecha = input("Escribe tu año de nacimiento: ")
+
+mensaje = f"Te llamas {nombre} naciste en {lugar} en {fecha}"
+
+print(mensaje)
